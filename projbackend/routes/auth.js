61a6c9d47018c728618e062e3fa2f37acc1ce252
Jeel Patel <jeeltpatel@gmail.com>
2021-06-26 14:16:59 +0530

    Authentication routes added

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,14 +2,27 @@ var express = require('express');
 const { sign } = require('jsonwebtoken');
 var router = express.Router()
 const {check} = require('express-validator');
-const {signout , signup} = require('../controllers/auth')
+const {signout , signup, signin ,isSignedIn} = require('../controllers/auth')
 
 
 router.post("/signup",[
     check("name","name should be at least 3 char").isLength({ min: 3 }),
     check("email","email is required").isEmail(),
-    check("passowrd","password should be at least 3 char").isLength({min:3})
+    check("password","password should be at least 3 char").isLength({min:3})
 ],signup)
+
+
+router.post("/signin",[
+    
+    check("email","email is required").isEmail(),
+    check("password","password field is required").isLength({min:1})
+],signin)
+
+
 router.get("/signout",signout)
+
+router.get("/testroute",isSignedIn,(req,res)=>{
+    res.send("A protected Route")
+})
 
 module.exports = router;
