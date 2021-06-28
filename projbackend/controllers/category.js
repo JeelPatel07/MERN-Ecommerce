bbda08b922d1e3f8507f74d54f7d8230b661302c
Jeel Patel <jeeltpatel@gmail.com>
2021-06-28 18:20:12 +0530

    Category Controllers added

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -28,3 +28,48 @@ exports.createCategory = (req,res) =>{
         res.json({category})
     })
 }
+
+exports.getCategory = (req,res) =>{
+ return res.json(req.category)
+}
+
+exports.getAllCategory = (req,res) =>{
+
+    Category.find().exec((err,categories)=>{
+        if(err){
+            return res.json(400).json({
+                error:"No categories found"
+            })
+        }
+        res.json(categories);
+    })
+
+}
+
+exports.updateCategory = (req,res) =>{
+    const category = req.category;
+    category.name = req.body.name;
+    
+    category.save((err,updateCategory)=>{
+        if(err){
+            return res.json(400).json({
+                error:"Failed to update the categories"
+            })
+        }
+        res.json(updateCategory);
+    })
+}
+
+exports.removeCategory = (req,res) => {
+    const category = req.category;
+    category.remove((err,category)=>{
+        if(err){
+            return res.json(400).json({
+                error:"Failed to delete this categories"
+            })
+        }
+        res.json({
+            message:`Successfully Deleted ${category}`
+        })
+    })
+}
--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getCategoryById,createCategory} = require('../controllers/category');
+const {getCategoryById,createCategory,getAllCategory,getCategory,updateCategory,removeCategory} = require('../controllers/category');
 const {isAdmin,isAuthenticated,isSignedIn} = require('../controllers/auth');
 const {getUserById} = require('../controllers/user');
 
@@ -13,5 +13,13 @@ router.param("categoryId",getCategoryById);
 router.post("/category/create/:userId",isSignedIn,isAuthenticated,isAdmin,createCategory)
 
 
+//read
+router.get("/category/:categoryId",getCategory);
+router.get("/categories",getAllCategory);
+
+//update
+router.put("/category/:categoryId/:userId",isSignedIn,isAuthenticated,isAdmin,updateCategory)
+//delete
+router.delete("/category/:categoryId/:userId",isSignedIn,isAuthenticated,isAdmin,removeCategory)
 
 module.exports = router;
