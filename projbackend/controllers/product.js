f7a8deb02dddfa6c0c68ca4a7f5924232846af98
Jeel Patel <jeeltpatel@gmail.com>
2021-06-29 13:08:01 +0530

    Product routes and controllers added

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,60 +1,94 @@
-const Product = require('../models/product');
-const formidable = require("formidable");
-const _ = require('lodash');
+const Product = require("../models/product");
+const _ = require("lodash");
 const fs = require("fs");
-const { formidable } = require('formidable');
+const formidable = require("formidable");
 
-
-exports.getProductById = (req,res,next,id) =>{
-    Product.findById(id)
+exports.getProductById = (req, res, next, id) => {
+  Product.findById(id)
     .populate("category")
-    .exec((err,product)=>{
-    if(err){
+    .exec((err, product) => {
+      if (err) {
         return res.status(400).json({
-            err:"Product not found"
-        })
-    }
-    req.product = product;
-    next();
-    })
-
-}
-exports.createProduct = (req,res) =>{
-
-  let form  = new formidable.IncomingForm();
+          err: "Product not found",
+        });
+      }
+      req.product = product;
+      next();
+    });
+};
+exports.createProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
   form.keepExtensions = true;
 
-  form.parse(req,(err,fields,file)=>{
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "problem with image"
+      });
+    }
+    //destructure the fields
+    const { name, description, price, category, stock } = fields;
+
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({
+        error: "Please include all fields"
+      });
+    }
+
+    let product = new Product(fields);
+    console.log(product )
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big!"
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    //save to the DB
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "Saving tshirt in DB failed"
+        });
+      }
+      res.json(product);
+    });
+  });
+};
+
+exports.getProduct = (req,res) =>{
+  req.product.photo = undefined;
+  return res.json(req.product);
+}
+
+exports.photo = (req,res,next) =>{
+  if(req.product.photo.data){
+    res.set("Content-Type",req.product.photo.contentType)
+    return res.send(req.product.photo.data)
+  }
+  next();
+}
+
+exports.deleteProduct = (req,res)=>{
+   const product = req.product
+   product.remove((err,deletedproduct)=>{
       if(err){
-          return res.status(400).json({
-              err:"Problem with image"
-          })
+        return res.status(400).json({
+          err:"Failed to delete the product"
+        })
       }
-
-      //restrictions on fields
-      let product = new Product(fields)
-
-
-      //handle file 
-      if(file.photo){
-          if(file.photo.size > 3000000){
-              return res.status(400).json({
-                  err:"File size too big!"
-              })
-          }
-          product.photo.data = fs.readFileSync(file.photo.path)
-          product.photo.contentType = file.photo.type
-      }
-      //save  to db
-      product.save((err,product)=>{
-          if(err){
-              return res.status(400).json({
-                  err:"Saving t-shirt in DB failed!!"
-              })
-          }
-          res.json(product);
+      res.json({
+        message:"Deletion was successful",deletedproduct
       })
-  })
+   })
+}
 
+exports.updateProduct = (req,res)=>{
+  const product = req.product;
 
 }
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,25 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById,createProduct} = require('../controllers/product');
-const {isAdmin,isAuthenticated,isSignedIn} = require('../controllers/auth');
-const {getUserById} = require('../controllers/user');
-
-
+const { getProductById, createProduct , getProduct , photo , deleteProduct , updateProduct} = require("../controllers/product");
+const { isAdmin, isAuthenticated, isSignedIn } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
 
 // params
-router.param("userId",getUserById)
-router.param("productId",getProductById)
+router.param("userId", getUserById);
+router.param("productId", getProductById);
 
 //routes
-router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct)
+// router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct)
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
+router.get("/product/:productId",getProduct)
+router.get("/product/photo/:productId",photo)
 
+router.delete("/product/:productId/:userId",isSignedIn,
+isAuthenticated,
+isAdmin,
+deleteProduct
+)
 
-
-
-
-
-
+router.put("/product/:productId/:userId",isSignedIn,
+isAuthenticated,
+isAdmin,
+updateProduct
+)
 
 module.exports = router;
