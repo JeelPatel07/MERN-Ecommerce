6f6c1524ed1a1b4466b1ef2c0723082f466cfb88
Jeel Patel <jeeltpatel@gmail.com>
2021-07-10 11:01:49 +0530

    Started with order routes and controllers

---
+++ projbackend/controllers/order.js
@@ -0,0 +1,17 @@
+const { Order, ProductCart } = require('../models/order')
+
+
+exports.getOrderById = (req,res,next,id)=>{
+    Order.findById(id)
+    .populate("products.product","name price")
+    .exec((err,order)=>{
+        if(err){
+            return res.status(400).json({
+                error:"No Order found in DB"
+            })
+        }
+        req.order = order;
+        next();
+    })
+
+}
---
+++ projbackend/routes/order.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const { isAdmin,isAuthenticated,isSignedIn } = require("../controllers/auth");
+const { getUserById,pushOrderInPurchaseList} = require("../controllers/user");
+const { updateStock} = require("../controllers/product");
+
+const {getOrderById} = require("../controllers/order")
+
+//params
+
+router.param("userId",getUserById)
+router.param("orderId",getOrderById);
+
+
+
+//Actual routes
+
+
+
+
+module.exports = router
