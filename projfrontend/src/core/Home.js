3ac7b1c2291096fa2e6bbd6882998c8681f3173f
Jeel Patel <jeeltpatel@gmail.com>
2021-07-26 10:53:05 +0530

    Added header and footer component

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import "../styles.css"
+import Base from "./Base"
 export default function Home(){
-
+  console.log("API IS ",process.env.REACT_APP_BACKEND)
     return (
-        <div>
-            <h1>Hello Front End</h1>
-        </div>
+        <Base title="Home Page"> 
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+            </div>
+        </Base>
     )
 }
