e80513ffcdbf432ed7fea3ea923e6b31ce0114f7
Jeel Patel <jeeltpatel@gmail.com>
2021-08-27 12:37:26 +0530

    product category in progress

---
+++ projfrontend/src/admin/ManageCategories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Base from '../core/Base'
+
+const  ManageCategories = ()=> {
+
+    return(
+
+        <Base title="Manage Categories" description="Welcome to Manage Product Categories Section">
+     <h1> ManageCategories</h1>
+        </Base>
+    )
+
+}
+
+export default ManageCategories;
